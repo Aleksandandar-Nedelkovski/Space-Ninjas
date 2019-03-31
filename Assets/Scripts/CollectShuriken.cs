56e1e4ebfe4dda9b7458386bf2ffa0cae79fe035
Kirill Yantikov <kirillyantikov@Kirills-MacBook-Pro.local>
2019-03-31 15:00:34 -0500

    add music, shurikens

--- Assets/Scripts/CollectShuriken.cs
+++ Assets/Scripts/CollectShuriken.cs
@@ -6,10 +6,13 @@ public class CollectShuriken : MonoBehaviour
 {
     // public AudioSource collectSound;
     void OnTriggerEnter2D (Collider2D other) {
+        if (other.name =="Player")
+        {
             Debug.Log("-------sadf-asdfdf------------");
             // collectSound.Play ();
             ScoringSystem.theScore += 50;
             Destroy (gameObject);
+        }
         // collectSound.Play ();
         // ScoringSystem.theScore += 50;
         // Destroy (gameObject);
